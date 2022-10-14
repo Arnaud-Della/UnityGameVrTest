cda18a7275b89fa6e016ad511887141ec5e3b728
Arnaud <arnaud.dellam@gmail.com>
2022-10-14 12:13:07 +0200

    Finalisastion mauvais endroit

--- Assets/Scenes/Scripts/Jeux/AvatarFactory.cs
+++ Assets/Scenes/Scripts/Jeux/AvatarFactory.cs
@@ -170,11 +170,6 @@ public class AvatarFactory : MonoBehaviour
             MyAvatarConfiguration = new AvatarConfiguration(AvatarPhotonView.ViewID, Vector3.zero, Url, avatar);
             IsMyOwnAvatar = false;
 
-            // Finalisation de la Creation de l'avatar
-            animator.avatar = SqueletteAvatarAnimator;
-            animator.runtimeAnimatorController = ControllerAnimator;
-            rigBuilder.Build();
-
             Debug.LogWarning("L'Avatar a été correctement creer");
             Debug.LogWarning($"INFO : {AvatarPhotonView.ViewID}, {avatar.transform.position}");
 
